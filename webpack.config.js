2f26e2875d46a447a82e675e16053e95d2571baa
Ronin Protheroe <rprotheroe@protonmail.com>
2020-07-22 10:11:52 -0500

    bruh

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: path.resolve(__dirname) + '/src/index.js',
   mode: "production",
   module: {
     rules: [
